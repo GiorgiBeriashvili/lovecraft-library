38bbc5b54c668233e7b63b81f6f5cc6266c9a271
uabua <mariami.chakhvadze.1@btu.edu.ge>
2020-06-30 20:50:23 +0400

    Rename MainActivity to LibraryActivity
    Additionally, rename activity's corresponding xml file to activity_library.

---
+++ app/src/main/java/dev/beriashvili/exams/lovecraftlibrary/activities/LibraryActivity.kt
@@ -0,0 +1,12 @@
+package dev.beriashvili.exams.lovecraftlibrary.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.beriashvili.exams.lovecraftlibrary.R
+
+class LibraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+    }
+}
